5105d9785b2b65ccad4a2726d9e9ffff34e99d2f
Akhilesh Bhatt <akhileshbhatt049@gmail.com>
2026-09-24 20:12:18 +0545

    Nav link adjusted

--- app/components/Topbar.tsx
+++ app/components/Topbar.tsx
@@ -15,7 +15,7 @@ const Topbar = () => {
 
           {/* Navigation */}
           <div className=" flex items-center gap-10">
-            <Link href="#" className="text-white px-4 py-2 rounded-lg hover:bg-orange-500 transition duration-300">
+            <Link href="/" className="text-white px-4 py-2 rounded-lg hover:bg-orange-500 transition duration-300">
               Home
             </Link>
 
