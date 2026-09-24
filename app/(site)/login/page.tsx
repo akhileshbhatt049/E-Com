5d9c143f10c062672023a262a2ebf0f20281b936
Akhilesh Bhatt <akhileshbhatt049@gmail.com>
2026-09-24 12:27:32 +0545

    login folder created

---
+++ app/(site)/login/page.tsx
@@ -0,0 +1,8 @@
+const Login = () => {
+  return (
+   <>
+    <h1>Loading...</h1>
+  </>
+  );
+};
+export default Login;
--- app/components/Topbar.tsx
+++ app/components/Topbar.tsx
@@ -17,7 +17,7 @@ const Topbar = () => {
       <div className="flex justify-center bg-orange-300 pb-3 gap-5 transition-all ">
         <input type="text" name="Search" placeholder="Search Product" className="border-3 border-yellow-300 focus:outline-violet-50 text-lg rounded-lg px-3 py-2 w-200 "/>
         <CiSearch className="text-4xl relative top-1 cursor-pointer"/>
-        <Link href="#" className="py-2 hover:bg-orange-400 px-2 h-10 rounded-lg self-center"> LOGIN</Link>
+        <Link href="/login" className="py-2 hover:bg-orange-400 px-2 h-10 rounded-lg self-center"> LOGIN</Link>
         <Link href="#" className="py-2 hover:bg-orange-400 px-2 h-10 rounded-lg self-center"> SIGN UP</Link>
       </div>
     </>
